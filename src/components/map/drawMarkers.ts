9f1179a78b9a8ce4df8ac16059fe31edca7b5151
Virian <gstok@poczta.fm>
2022-07-31 18:10:16 +0200

    feat: change the way markers are layered

--- src/components/map/drawMarkers.ts
+++ src/components/map/drawMarkers.ts
@@ -1,9 +1,19 @@
 import { MutableRefObject } from 'react';
+import sortBy from 'lodash/sortBy';
 
 import markersData from '../../assets/markers.json';
 
 import { Coords, Marker, LoadedMarkers, MarkerCategories } from './types';
 import { MARKER_SIZE } from './constants';
+
+// markers that are lower on the screen will be layered on top of those being
+// higher in case they overlap
+const sortedMarkersData = sortBy(
+  Object.entries(markersData).flatMap(([category, data]) =>
+    data.markers.map((marker) => ({ ...marker, category }))
+  ),
+  ['y', 'x']
+);
 
 interface DrawMarkersParams {
   context: CanvasRenderingContext2D;
@@ -24,59 +34,56 @@ const drawMarkers = ({
 
   const drawnMarkers: Marker[] = [];
 
-  Object.entries(markersData).forEach(([category, data]) => {
+  sortedMarkersData.forEach(({ x, y, label, category }) => {
     if (!filters[category]) {
       return;
     }
 
     const loadedMarker = LoadedMarkersRef.current[category as MarkerCategories];
+    const shouldDrawMarker =
+      mapCoordOffset.x - MARKER_SIZE * zoomLevel.scale < x && // checking left edge of the screen
+      mapCoordOffset.x +
+        window.innerWidth * zoomLevel.scale +
+        MARKER_SIZE * zoomLevel.scale >
+        x && // right edge
+      mapCoordOffset.y - MARKER_SIZE * zoomLevel.scale < y && // top edge
+      mapCoordOffset.y +
+        window.innerHeight * zoomLevel.scale +
+        MARKER_SIZE * zoomLevel.scale >
+        y; // bottom edge
 
-    data.markers.forEach(({ x, y, label }) => {
-      const shouldDrawMarker =
-        mapCoordOffset.x - MARKER_SIZE * zoomLevel.scale < x && // checking left edge of the screen
-        mapCoordOffset.x +
-          window.innerWidth * zoomLevel.scale +
-          MARKER_SIZE * zoomLevel.scale >
-          x && // right edge
-        mapCoordOffset.y - MARKER_SIZE * zoomLevel.scale < y && // top edge
-        mapCoordOffset.y +
-          window.innerHeight * zoomLevel.scale +
-          MARKER_SIZE * zoomLevel.scale >
-          y; // bottom edge
+    if (shouldDrawMarker) {
+      const markerScreenX =
+        (x - mapCoordOffset.x) / zoomLevel.scale - MARKER_SIZE / 2;
+      const markerScreenY =
+        (y - mapCoordOffset.y) / zoomLevel.scale - MARKER_SIZE / 2;
 
-      if (shouldDrawMarker) {
-        const markerScreenX =
-          (x - mapCoordOffset.x) / zoomLevel.scale - MARKER_SIZE / 2;
-        const markerScreenY =
-          (y - mapCoordOffset.y) / zoomLevel.scale - MARKER_SIZE / 2;
+      drawnMarkers.push({ x: markerScreenX, y: markerScreenY, label });
 
-        drawnMarkers.push({ x: markerScreenX, y: markerScreenY, label });
+      if (loadedMarker) {
+        context.drawImage(
+          loadedMarker,
+          markerScreenX,
+          markerScreenY,
+          MARKER_SIZE,
+          MARKER_SIZE
+        );
+      } else {
+        const img = new Image();
+        img.src = require(`../../assets/icons/icon-${category}.webp`).default;
+        img.onload = () => {
+          LoadedMarkersRef.current[category as MarkerCategories] = img;
 
-        if (loadedMarker) {
           context.drawImage(
-            loadedMarker,
+            img,
             markerScreenX,
             markerScreenY,
             MARKER_SIZE,
             MARKER_SIZE
           );
-        } else {
-          const img = new Image();
-          img.src = require(`../../assets/icons/icon-${category}.webp`).default;
-          img.onload = () => {
-            LoadedMarkersRef.current[category as MarkerCategories] = img;
-
-            context.drawImage(
-              img,
-              markerScreenX,
-              markerScreenY,
-              MARKER_SIZE,
-              MARKER_SIZE
-            );
-          };
-        }
+        };
       }
-    });
+    }
   });
 
   return drawnMarkers;
