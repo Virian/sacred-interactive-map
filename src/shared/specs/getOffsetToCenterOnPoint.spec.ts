2f4b06f31b993349710f40c109640b4665545a11
Virian <gstok@poczta.fm>
2022-08-05 00:58:12 +0200

    test: add tests for getOffsetToCenterOnPoint function

---
+++ src/shared/specs/getOffsetToCenterOnPoint.spec.ts
@@ -0,0 +1,45 @@
+import getOffsetToCenterOnPoint from '../getOffsetToCenterOnPoint';
+import { Coords } from '../../types';
+
+jest.mock('../../constants', () => ({
+  MAP_WIDTH: 20000,
+  MAP_HEIGHT: 10000,
+}));
+
+const cases: [Coords, number | undefined, Coords][] = [
+  [{ x: 6500, y: 5000 }, 1, { x: 5540, y: 4460 }],
+  [{ x: 6500, y: 5000 }, undefined, { x: 5540, y: 4460 }],
+  [{ x: 100, y: 200 }, 1, { x: 0, y: 0 }],
+  [{ x: 19800, y: 9900 }, 1, { x: 18080, y: 8920 }],
+  [{ x: -100, y: -100 }, 1, { x: 0, y: 0 }],
+  [{ x: 6500, y: 5000 }, 5, { x: 1700, y: 2300 }],
+  [{ x: 100, y: 200 }, 5, { x: 0, y: 0 }],
+  [{ x: 19800, y: 9900 }, 5, { x: 10400, y: 4600 }],
+  [{ x: -100, y: -100 }, 5, { x: 0, y: 0 }],
+];
+
+describe('getMarkerFromSearchParams', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'innerWidth', {
+      writable: true,
+      configurable: true,
+      value: 1920,
+    });
+    Object.defineProperty(window, 'innerHeight', {
+      writable: true,
+      configurable: true,
+      value: 1080,
+    });
+  });
+
+  it.each(cases)(
+    'should return correct map offset to center on point %o with scale %d',
+    (pointCoords, zoomScaleNumber, expectedResult) => {
+      // when
+      const result = getOffsetToCenterOnPoint(pointCoords, zoomScaleNumber);
+
+      // then
+      expect(result).toStrictEqual(expectedResult);
+    }
+  );
+});
