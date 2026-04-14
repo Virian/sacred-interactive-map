8de7fa1df0872fa06fc92d2eee082b3f15a1944d
Virian <gstok@poczta.fm>
2026-04-14 20:49:27 +0200

    build: bump dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, mergeConfig } from 'vite';
 import { defineConfig as defineConfigVitest } from 'vitest/config';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 const viteConfig = defineConfig({
